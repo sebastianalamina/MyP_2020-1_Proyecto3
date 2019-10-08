e2f6df3cdbf24ef6fac99dcb87bed6301812ee7d
sebastianalamina <alamina@ciencias.unam.mx>
2019-10-07 21:16:29 -0500

    Clase Rey - Clase reescrita en Ruby.

--- code/piezas/Rey.rb
+++ code/piezas/Rey.rb
@@ -3,6 +3,69 @@ require_relative '../Pieza'
 # Clase que representa a la pieza Rey.
 class Rey < Pieza
 
+  # Inicializador de un Rey.
+  # columna:: Columna del Rey.
+  # renglón:: Renglón del Rey.
+  # color:: Color del Rey (booleano).
+  def initialize(columna, renglón, color)
+    super(columna, renglón, color);
+  end
 
+  def posiblesMovimientos
+    # Lista con los posibles movimientos
+    # de la pieza.
+    listaPM = []
+
+    # NORTE.
+    begin
+      listaPM.push( desplazar(0, 1) )
+    rescue
+    end
+
+    # NORESTE.
+    begin
+      listaPM.push( desplazar(1, 1) )
+    rescue
+    end
+
+    # ESTE.
+    begin
+      listaPM.push( desplazar(1, 0) )
+    rescue
+    end
+
+    # SURESTE.
+    begin
+      listaPM.push( desplazar(1, -1) )
+    rescue
+    end
+
+    # SUR.
+    begin
+      listaPM.push( desplazar(0, -1) )
+    rescue
+    end
+
+    # SUROESTE.
+    begin
+      listaPM.push( desplazar(-1, -1) )
+    rescue
+    end
+
+    # OESTE.
+    begin
+      listaPM.push( desplazar(-1, 0) )
+    rescue
+    end
+
+    # NOROESTE.
+    begin
+      listaPM.push( desplazar(-1, 1) )
+    rescue
+    end
+
+    # Se regresa la lista...
+    return listaPM
+  end
 
 end
