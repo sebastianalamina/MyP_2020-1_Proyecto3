8ed2bc03fe661d7173d16481d42a336ca597c9a4
sebastianalamina <alamina@ciencias.unam.mx>
2019-10-07 21:14:28 -0500

    Clase Alfil - Clase reescrita en Ruby.

--- code/piezas/Alfil.rb
+++ code/piezas/Alfil.rb
@@ -3,6 +3,69 @@ require_relative '../Pieza'
 # Clase que representa a la pieza Alfil.
 class Alfil < Pieza
 
+  # Inicializador de un Alfil.
+  # columna:: Columna del Alfil.
+  # renglón:: Renglón del Alfil.
+  # color:: Color del Alfil (booleano).
+  def initialize(columna, renglón, color)
+    super(columna, renglón, color);
+  end
 
+  def posiblesMovimientos
+    # Lista con los posibles movimientos
+    # de la pieza.
+    listaPM = []
+
+    # NORESTE.
+    begin
+      x = 1
+      y = 1
+      7.times do
+        listaPM.push( desplazar(x, y) )
+        x += 1
+        y += 1
+      end
+    rescue
+    end
+
+    # SURESTE.
+    begin
+      x = 1
+      y = -1
+      7.times do
+        listaPM.push( desplazar(x, y) )
+        x += 1
+        y -= 1
+      end
+    rescue
+    end
+
+    # SUROESTE.
+    begin
+      x = -1
+      y = -1
+      7.times do
+        listaPM.push( desplazar(x, y) )
+        x -= 1
+        y -= 1
+      end
+    rescue
+    end
+
+    # NOROESTE.
+    begin
+      x = -1
+      y = 1
+      7.times do
+        listaPM.push( desplazar(x, y) )
+        x -= 1
+        y += 1
+      end
+    rescue
+    end
+
+    # Se regresa la lista...
+    return listaPM
+  end
 
 end
