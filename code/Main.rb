422630c2138c9410ecb6d30e90c0042d6e08e975
sebastianalamina <alamina@ciencias.unam.mx>
2019-10-12 17:27:47 -0500

    Main modificado.

--- code/Main.rb
+++ code/Main.rb
@@ -3,11 +3,14 @@ x = File.join(__dir__, 'piezas', '*.rb')
 Dir[x].each do |file|
   require_relative file
 end
+# "require" de AjedrezInterfaz.rb (código interfaz).
+require_relative './interfaz/AjedrezInterfaz'
+require_relative './Tablero'
 
 # El código dentro de este método sólo se ejecuta
 # si este archivo se llama desde la terminal (no
 # se ejecuta si se requiere en otro archivo).
 # Por lo que se toma como el método Main.
 if __FILE__ == $0
-  puts "Aquí va el main jeje"
+  interfaz()
 end
